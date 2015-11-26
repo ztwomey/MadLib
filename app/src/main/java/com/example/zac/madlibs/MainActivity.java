312a8575d1b28995720a77ad206d57fa141118a0
ztwomey <zrt685@hotmail.com>
2015-11-25 21:38:07 -0500

    Added variables to StoryActivity.java and added OnClickListener to MainActivity.java

--- app/src/main/java/com/example/zac/madlibs/MainActivity.java
+++ app/src/main/java/com/example/zac/madlibs/MainActivity.java
@@ -5,20 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button startStory;
+    public Button mStartStory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startStory = (Button)findViewById(R.id.startStory);
+        mStartStory = (Button)findViewById(R.id.startStory);
 
-
+        mStartStory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/zac/madlibs/StoryActivity.java
+++ app/src/main/java/com/example/zac/madlibs/StoryActivity.java
@@ -4,13 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class StoryActivity extends AppCompatActivity {
+
+    public EditText mFirstWord;
+    public EditText mSecondWord;
+    public EditText mThirdWord;
+    public EditText mFourthWord;
+    public EditText mFifthWord;
+    public EditText mSixthWord;
+    public EditText mSeventhWord;
+    public EditText mEighthWord;
+    public EditText mNinthWord;
+    public Button mCreateStory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        mFirstWord = (EditText)findViewById(R.id.firstWord);
+        mSecondWord = (EditText)findViewById(R.id.secondWord);
+        mThirdWord = (EditText)findViewById(R.id.thirdWord);
+        mFourthWord = (EditText)findViewById(R.id.fourthWord);
+        mFifthWord = (EditText)findViewById(R.id.fifthWord);
+        mSixthWord = (EditText)findViewById(R.id.sixthWord);
+        mSeventhWord = (EditText)findViewById(R.id.seventhWord);
+        mEighthWord = (EditText)findViewById(R.id.eighthWord);
+        mNinthWord = (EditText)findViewById(R.id.ninthWord);
+        mCreateStory = (Button)findViewById(R.id.createStoryButton);
     }
 
 }
