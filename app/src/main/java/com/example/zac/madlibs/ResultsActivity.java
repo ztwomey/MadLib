25162161740d77c2182a8a0e9541fda257ccabe5
ztwomey <zrt685@hotmail.com>
2015-11-25 22:19:58 -0500

    Added story and strings to get the user words. And intents to send the words the user input to the final story page

--- app/src/main/java/com/example/zac/madlibs/ResultsActivity.java
+++ app/src/main/java/com/example/zac/madlibs/ResultsActivity.java
@@ -1,15 +1,28 @@
 package com.example.zac.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    public TextView mStoryResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        mStoryResults = (TextView)findViewById(R.id.storyResults);
+        Intent intent = getIntent();
+
+        String storyText = intent.getStringExtra("key");
+
+        mStoryResults.setText(storyText);
     }
 }
--- app/src/main/java/com/example/zac/madlibs/StoryActivity.java
+++ app/src/main/java/com/example/zac/madlibs/StoryActivity.java
@@ -1,9 +1,12 @@
 package com.example.zac.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,6 +23,8 @@ public class StoryActivity extends AppCompatActivity {
     public EditText mNinthWord;
     public Button mCreateStory;
 
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +40,39 @@ public class StoryActivity extends AppCompatActivity {
         mEighthWord = (EditText)findViewById(R.id.eighthWord);
         mNinthWord = (EditText)findViewById(R.id.ninthWord);
         mCreateStory = (Button)findViewById(R.id.createStoryButton);
+
+        Intent intent = getIntent();
+
+        mCreateStory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getFirstWord = mFirstWord.getText().toString();
+                String getSecondWord = mSecondWord.getText().toString();
+                String getThirdWord = mThirdWord.getText().toString();
+                String getFourthWord = mFourthWord.getText().toString();
+                String getFifthWord = mFifthWord.getText().toString();
+                String getSixthWord = mSixthWord.getText().toString();
+                String getSeventhWord = mSeventhWord.getText().toString();
+                String getEighthWord = mEighthWord.getText().toString();
+                String getNinthWord = mNinthWord.getText().toString();
+
+                String story = String.format("You and your friends are wanting to see a(n) %s " +
+                                "movie and decide to head to the theater to see what is playing. You get there " +
+                                "and notice there are only %s choices. You all decide to see %s of " +
+                                "%s %s. There are plenty of seats available but you all decide to sit " +
+                                "in row %s. The movie is %s and everybody let out a %s at the end. " +
+                                "Overall, it was a %s trip to the movies.", getFirstWord, getSecondWord, getThirdWord, getFourthWord,
+                        getFifthWord, getSixthWord, getSeventhWord, getEighthWord, getNinthWord);
+
+                Intent sendStory = new Intent(StoryActivity.this, ResultsActivity.class);
+                sendStory.putExtra("key", story);
+                startActivity(sendStory);
+
+                Log.d(TAG, story.toString());
+            }
+        });
+
+
     }
 
 }
